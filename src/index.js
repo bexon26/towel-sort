b5882ab7a18c6696395bff3429b4447cd1869d4a
bexon26 <47786103+bexon26@users.noreply.github.com>
2020-02-26 21:05:40 +0300

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+ module.exports = function towelSort (matrix) {
+    var sortMass = [];
+    if (matrix == undefined || matrix.length == 0){
+        return [];   
+    } else {
+        for (var i = 0; i<matrix.length; i++){
+            if (i % 2 === 0) {
+                for(var j = 0; j<matrix[i].length; j++){
+                    sortMass.push(matrix[i][j]);
+                };
+            } else {
+                for(var j = matrix[i].length-1; j>=0; j--){
+                    sortMass.push(matrix[i][j]);
+                }
+            };
+        
+        };
+        return sortMass;
+    }
 }
+
